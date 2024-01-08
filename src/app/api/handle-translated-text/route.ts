861f3222fef65c90f8dbae556cb4b4e9cb709546
jay27694 <jaypatel27694@gmail.com>
2024-01-08 13:59:51 -0600

    fix api params type

--- src/app/api/handle-translated-text/route.ts
+++ src/app/api/handle-translated-text/route.ts
@@ -1,18 +1,16 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
   if (req.method === 'POST') {
     try {
       const data = await new Response(req.body).json()
       console.log('Received data:', data);
-      return NextResponse.json(data, { status: 200 });
-     ;
+      return NextResponse.json(data, { status: 200 });     
     } catch (error) {
       console.error('Error processing data:', error);
-      return NextResponse.json({ error: 'Internal Server Error' }, { status: 200 });      
+      return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });      
     }
   } else {    
-    res.status(405).json({ error: 'Method Not Allowed' });
+    return NextResponse.json({ error: 'Method Not Allowed' }, { status: 405 });   
   }
 }
